66c3b6a356eff23b5799c3abafb8698e8e3b33c5
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2023-10-15 01:02:35 -0700

    betty style

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -6,7 +6,8 @@
  * Return: always 0
  */
 
-int main(void) {
-    puts("Programming is like building a multilingual puzzle");
+int main(void) 
+{
+    puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }

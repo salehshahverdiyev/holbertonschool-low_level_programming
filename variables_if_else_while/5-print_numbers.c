be250cb68e01ae0369e3ecd0b1b6a76d89645056
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2023-10-17 02:36:18 -0700

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%i", a);
+	printf("\n");
+	return (0);
+}

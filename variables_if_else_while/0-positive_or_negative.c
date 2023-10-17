54287d198c8c54ba9271a1f0e682bc6d74fddbb8
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2023-10-17 00:38:17 -0700

    Betty is a big problemmmmmm,Positive anything is better than negative nothing

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d\n is positive", n);
-	if (n < 0)
-		printf("%d\n is negative", n);
-	else
-		printf("%d\n is zero", n);
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
 	return (0);
 }

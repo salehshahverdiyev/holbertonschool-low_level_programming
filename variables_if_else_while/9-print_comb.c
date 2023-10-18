bdbf5e18abde1da70a99d9d39615ac607d42e666
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2023-10-18 02:52:54 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9' ; a++)
+	{
+		putchar(a);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

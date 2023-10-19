f791b3e428f59eb2c3e064c0d281f80d7b8ddf5a
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2023-10-19 01:04:45 -0700

    i am confused

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - it prints alphabet
+ *
+ * Return: Always Alphabet
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}

7353606917a6033895614a04e902af907f226725
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2023-10-18 23:51:13 -0700

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int i;
+	char  _putchar[] = "_putchar";
+
+	for (i = 0 ;i <= 7; i++)
+		putchar(_putchar[i]);
+	putchar('\n');
+
+	return (0);
+}

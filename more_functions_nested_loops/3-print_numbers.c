505eeade1b00829472bd75e0dd1a2a0cc4dcffd3
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2023-10-22 10:04:49 -0700

    The numbers speak for themselves

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - from 0 to 9
+ * Description: print numbers
+ * Return: IDK
+*/
+void print_numbers(void)
+{
+	char i = '0';
+
+	while (i <= '9')
+	{
+		_putchar(i);
+		i++; 
+	}
+	_putchar('\n');
+}

5c9220879836d1c44d34331a3875b3e1975855aa
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2023-10-25 01:09:35 -0700

    98 Battery st

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - reset value to 98
+ * @n: parametr n
+ * Description: the function resets value to 98
+ * Return: IDK
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

8f7bc8e8c129ee7d443c3f78688bed6ac673b743
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2023-11-02 00:25:00 -0700

    returns the natural square root of a number

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,6 +1,25 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * _sqrt_help - checks all conditions for help
+ * @n: parametr n
+ * @i: parametr i
+ * Description: The function checks all conditions for help
+ * Return: square root and (i+1)
+*/
+int _sqrt_help(int n, int i)
+{
+	if (n == 0 || n == 1)
+		return (n);
+	if (n < 0)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	else if (i * i > n)
+		return (-1);
+	return (_sqrt_help(n, i + 1));
+}
+/**
  * _sqrt_recursion -  returns the natural square root of a number
  * @n: parametr n
  * Description: The function returns square root of number or -1
@@ -8,19 +27,5 @@
 */
 int _sqrt_recursion(int n)
 {
-	int i = 1;
-
-	if (n == 0 || n == 1)
-		return (n);
-	if (n < 0)
-		return (-1);
-	while (i * i != n)
-	{
-		i++;
-		if (i * i == n)
-			return (i);
-		else if (i * i > n)
-			return (-1);
-	}
-	return (_sqrt_recursion(n));
+	return (_sqrt_help(n, 1));
 }

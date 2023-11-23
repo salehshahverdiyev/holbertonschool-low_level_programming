1fade7951704b7aa492223d26ec261e46f75cc42
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2023-11-23 00:02:04 -0800

    Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - struct
+ * @name: parametr name
+ * @owner: parametr owner
+ * @age: parametr age
+*/
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+#endif

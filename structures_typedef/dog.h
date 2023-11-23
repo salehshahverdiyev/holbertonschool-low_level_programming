3aef8f7a1c6c3b2344a051ac0aa1c49c4bb359f3
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2023-11-23 00:41:07 -0800

    Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,12 +6,12 @@
  * @owner: parametr owner
  * @age: parametr age
 */
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif

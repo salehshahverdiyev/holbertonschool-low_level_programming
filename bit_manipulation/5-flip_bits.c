0d544ddadfbd1985b8d5d2d548550ee95febc347
salehshahverdiyev <salehsahverdiyev201@gmail.com>
2023-12-10 12:10:10 -0800

    fixing betty error

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -3,7 +3,7 @@
  * flip_bits - returns the number of bits
  * @n: parametr n
  * @m: parametr m
- * Description: The function returns the number of bits you would need 
+ * Description: The function returns the number of bits you would need
  * to flip to get from one number to another
  * Return: IDK
 */
